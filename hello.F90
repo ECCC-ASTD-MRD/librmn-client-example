4d90a5dfaf60b2a7b5c9cf141d29086f5e24e7c7
Samuel Gilbert <samuel.gilbert@canada.ca>
2021-10-14 16:28:52 -0400

    Initial commit

---
+++ hello.F90
@@ -0,0 +1,17 @@
+module librmn
+    use iso_c_binding
+    implicit none
+
+    interface
+        subroutine print_librmn_version() bind(C, name = 'print_librmn_version')
+        end subroutine print_librmn_version
+    end interface
+end module
+
+
+program hello
+    use librmn
+    implicit none
+
+    call print_librmn_version()
+end program hello
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <librmn.h>
+
+int main(int argc, char **argv) {
+    print_librmn_version();
+    return 0;
+}
